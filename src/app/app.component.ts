1f72a62f145da8fdc26912102ba6f60a355e928e
chai <chaithra.amin98@gmail.com>
2022-09-28 13:11:55 +0530

    sticky navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sticky-navbar';
+  ngOnInit(){
+    var menus:any=document.getElementById("menus");
+    var navbar:any=document.getElementById("navbar");
+    window.onscroll=function(){
+      if(window.pageYOffset >= menus.offsetTop)
+      {
+        navbar.classList.add("sticky");
+      }
+      else{
+        navbar.classList.remove("sticky");
+      }
+    }
+  }
 }
